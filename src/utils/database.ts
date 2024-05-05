31cace8b6f2229635859fa66994d01d08396d4e6
soojjung <89066676+soojjung@users.noreply.github.com>
2024-05-05 23:04:53 +0900

    Update database.ts

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -5,8 +5,6 @@ const connect = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI || "", {});
-    // await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Mongo Connection successfully established.");
   } catch (error) {
     throw new Error("Error connecting to Mongoose");
   }
